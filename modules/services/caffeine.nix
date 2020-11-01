fd5f35c5cf7024faa16aaf6126b12b752abf01da
Nikita Uvarov <uv.nikita@gmail.com>
2020-11-01 12:28:27 +0100

    caffeine: add service

---
+++ modules/services/caffeine.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.services.caffeine;
+
+in {
+  meta.maintainers = [ maintainers.uvnikita ];
+
+  options = {
+    services.caffeine = { enable = mkEnableOption "Caffeine service"; };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.user.services.caffeine = {
+      Unit = { Description = "caffeine"; };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+
+      Service = {
+        Restart = "on-failure";
+        PrivateTmp = true;
+        ProtectSystem = "full";
+        ExecStart = "${pkgs.caffeine-ng}/bin/caffeine";
+      };
+    };
+  };
+}
