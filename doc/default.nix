91ef21740f06aac9941fd3d7fecd8d1fbd1bf87e
Robert Helgesson <robert@rycee.net>
2021-01-18 23:21:32 +0100

    docs: NixOS and nix-darwin option documentation

--- doc/default.nix
+++ doc/default.nix
@@ -38,15 +38,87 @@ let
     docBook.id = "home-manager-options";
   };
 
+  nixosModuleDocs = nmd.buildModulesDocs {
+    modules = let
+      nixosModule = module: pkgs.path + "/nixos/modules" + module;
+      mockedNixos = with lib; {
+        options = {
+          environment.pathsToLink = mkOption {
+            type = types.listOf types.str;
+            visible = false;
+          };
+          systemd.services = mkOption {
+            type = types.attrsOf types.anything;
+            visible = false;
+          };
+          users.users = mkOption {
+            type = types.attrsOf types.anything;
+            visible = false;
+          };
+        };
+      };
+    in [
+      ../nixos/default.nix
+      mockedNixos
+      (nixosModule "/misc/assertions.nix")
+      scrubbedPkgsModule
+    ];
+    moduleRootPaths = [ ./.. ];
+    mkModuleUrl = path:
+      "https://github.com/nix-community/home-manager/blob/master/${path}#blob-path";
+    channelName = "home-manager";
+    docBook = {
+      id = "nixos-options";
+      optionIdPrefix = "nixos-opt";
+    };
+  };
+
+  nixDarwinModuleDocs = nmd.buildModulesDocs {
+    modules = let
+      nixosModule = module: pkgs.path + "/nixos/modules" + module;
+      mockedNixDarwin = with lib; {
+        options = {
+          environment.pathsToLink = mkOption {
+            type = types.listOf types.str;
+            visible = false;
+          };
+          system.activationScripts.postActivation.text = mkOption {
+            type = types.str;
+            visible = false;
+          };
+          users.users = mkOption {
+            type = types.attrsOf types.anything;
+            visible = false;
+          };
+        };
+      };
+    in [
+      ../nix-darwin/default.nix
+      mockedNixDarwin
+      (nixosModule "/misc/assertions.nix")
+      scrubbedPkgsModule
+    ];
+    moduleRootPaths = [ ./.. ];
+    mkModuleUrl = path:
+      "https://github.com/nix-community/home-manager/blob/master/${path}#blob-path";
+    channelName = "home-manager";
+    docBook = {
+      id = "nix-darwin-options";
+      optionIdPrefix = "nix-darwin-opt";
+    };
+  };
+
   docs = nmd.buildDocBookDocs {
     pathName = "home-manager";
-    modulesDocs = [ hmModulesDocs ];
+    modulesDocs = [ hmModulesDocs nixDarwinModuleDocs nixosModuleDocs ];
     documentsDirectory = ./.;
     documentType = "book";
     chunkToc = ''
       <toc>
         <d:tocentry xmlns:d="http://docbook.org/ns/docbook" linkend="book-home-manager-manual"><?dbhtml filename="index.html"?>
           <d:tocentry linkend="ch-options"><?dbhtml filename="options.html"?></d:tocentry>
+          <d:tocentry linkend="ch-nixos-options"><?dbhtml filename="nixos-options.html"?></d:tocentry>
+          <d:tocentry linkend="ch-nix-darwin-options"><?dbhtml filename="nix-darwin-options.html"?></d:tocentry>
           <d:tocentry linkend="ch-tools"><?dbhtml filename="tools.html"?></d:tocentry>
           <d:tocentry linkend="ch-release-notes"><?dbhtml filename="release-notes.html"?></d:tocentry>
         </d:tocentry>
